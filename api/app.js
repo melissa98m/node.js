08f6d4cb27b71d35861426cb553a967aed01b598
melissa98m <melissa.mangione@gmail.com>
2022-01-10 17:26:35 +0100

    gestion de parametre avec GET

--- api/app.js
+++ api/app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+
+
+
+const users = [
+    {
+        id: 1,
+        name: "Mélissa"
+    },
+    {
+        id:2,
+        name: "John"
+    },
+    {
+        id:3,
+        name: "Fabien"
+    }
+]
+
 //middlewere programe qui s'execute avant le reste
 /*app.use((req , res , next) => {
     console.log('URL :' + req.url) //retourne dans le terminale l'url de la requete
     next();
-})*/
+})
 app.use(morgan('dev'))
 
 app.get('/api', (req, res) => {
@@ -23,6 +41,26 @@ app.get('/api/v1/user/:id', (req, res) => {
 app.listen('8080', () => {
     console.log('listening on 8080')
 })
+*/
+
+app.use(morgan('dev'))
+
+app.get('/api/v1/users/:id' , (req , res) => {
+    res.send(users[(req.params.id)-1].name)
+})
+
+app.get('/api/v1/users' , (req , res)=>{
+    if(req.query.max != undefined && req.query.max > 0){
+        res.send(users.slice(0, req.query.max)) //permet de mettre un nombre de resultat voulu
+
+    }else {
+        res.send(users)
+    }
+})
+app.listen('8080', () => {
+    console.log('listening on 8080')
+})
+
 
 
 
