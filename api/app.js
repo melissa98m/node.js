d7c8d9a060bce5d0d913b7f098f4995ef449f578
melissa98m <melissa.mangione@gmail.com>
2022-01-10 16:58:21 +0100

    ajout de parametre en get

--- api/app.js
+++ api/app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.get('/api' , (req , res) => {
+app.get('/api', (req, res) => {
     res.send('Root API')
 })
 
-app.get('/api/v1' , (req , res) => {
+app.get('/api/v1', (req, res) => {
     res.send('API Version 1')
 })
 
-app.listen('8080' , ()=>{
+app.get('/api/v1/user/:id', (req, res) => {
+    res.send(req.params)
+})
+
+app.listen('8080', () => {
     console.log('listening on 8080')
 })
 
