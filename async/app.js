1cf94ff036e6ce51c879ad8292a69bf91b2d4eed
melissa98m <melissa.mangione@gmail.com>
2022-01-13 11:39:06 +0100

    les promise

--- async/app.js
+++ async/app.js
@@ -1,4 +1,4 @@
-//Asychrome gerer les asy
+/*Asychrome gerer les asy
 console.log("Debut")
 //Callbacks (fonction executer dés que la tache est finie) = code imbriqué
 getUser((user) => {
@@ -7,8 +7,6 @@ getUser((user) => {
         console.log(articles)
     })
 })
-//Promise
-//Async/Await
 console.log("Fin")
 
 function getUser(next) {
@@ -21,4 +19,21 @@ function getArticles(user, next) {
     setTimeout(() => {
         next([1, 2, 3])
     }, 1500)
-}
+
+*/
+/*Promise ca va executer certaine tache et une fois quelles sont realiser on passe a une autre mais si il y a une
+ erreur on passe a une tache special pour l'erreur
+*/
+console.log("Début")
+ new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("All good")
+        //reject(new Error("Error during"))
+    }, 1500)
+})
+    .then(message => console.log(message))//si tout va bien on execute then
+    .catch(err => console.log(err.message))//sinon erreur catch
+console.log("Fin")
+
+//Async/Await
+
