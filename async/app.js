0401e5ee03423212a8d44c0a78b315be286f78a9
melissa98m <melissa.mangione@gmail.com>
2022-01-13 13:07:19 +0100

    promesse en parallele

--- async/app.js
+++ async/app.js
@@ -1,6 +1,6 @@
 /*Asychrome gerer les asy
 console.log("Debut")
-//Callbacks (fonction executer dés que la tache est finie) = code imbriqué
+//CALLBACKS (fonction executer dés que la tache est finie) = code imbriqué
 getUser((user) => {
     console.log(user)
     getArticles(user, (articles) => {
@@ -24,8 +24,10 @@ function getArticles(user, next) {
 /*Promise ca va executer certaine tache et une fois quelles sont realiser on passe a une autre mais si il y a une
  erreur on passe a une tache special pour l'erreur
 */
+//UTILISATION D'UNE PROMESSE
+/*
 console.log("Début")
-/* new Promise((resolve, reject) => {
+ new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("All good")
         //reject(new Error("Error during"))
@@ -33,6 +35,9 @@ console.log("Début")
 })
     .then(message => console.log(message))//si tout va bien on execute then
     .catch(err => console.log(err.message))//sinon erreur catch*/
+//UTILISATION DE PLUSIEURS PROMESSES A LA SUITE
+/*
+console.log("Début")
 getUser()
     .then(user => getArticles(user))
     .then(articles => console.log(articles))//On peut mettre plusieurs promesses a la suite elle s'executeront
@@ -46,15 +51,33 @@ function getUser() {
         }, 1500)
     })
 }
-
 function getArticles(user) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             reject(new Error("Error during getArticles()"))
-           resolve([1, 2, 3])
+            resolve([1, 2, 3])
         }, 1500)
     })
 }
+console.log("Fin")
+*/
+
+//UTILISATION DE PROMESSE EN PARALLELE
+console.log("Début")
+let p1 = new Promise((resolve, reject) => {
+    setTimeout(() =>{
+        resolve("promise 1")
+    },1500)
+})
+let p2 = new Promise((resolve, reject) => {
+    setTimeout(() =>{
+        resolve("p2")
+    },3000)
+})
+Promise.all([p1,p2])  //.all va attendre toute les promise avant le resultat (tout sera dans un array)
+//Promise.race([p1,p2])// le resultat sera la premiere promise a avoir été finie celle qui dure le moins longtemps
+    .then(result => console.log(result))
+
 console.log("Fin")
 
 //Async/Await
