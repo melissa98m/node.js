d3c67a08465f3884ae4fb5a4f4ef50ed31604576
melissa98m <melissa.mangione@gmail.com>
2022-01-13 10:40:54 +0100

    Routing with express

--- api/app.js
+++ api/app.js
@@ -42,104 +42,96 @@ app.listen('8080', () => {
     console.log('listening on 8080')
 })
 */
+let UsersRouter = express.Router()
 
 app.use(morgan('dev'))
-
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({extended: true})) // for parsing application/x-www-form-urlencoded
 
-//GET
-app.get('/api/v1/users/:id', (req, res) => {
+UsersRouter.route('/')
+    //GET all user
+    .get((req, res) => {
+        if (req.query.max != undefined && req.query.max > 0) {
+            res.json(success(users.slice(0, req.query.max))) //permet de mettre un nombre de resultat voulu
 
-    let index = getIndex(req.params.id);
-    if (typeof (index) == 'string') {
-        res.json(error(index))
-    } else {
-        res.json(success(users[index]))
-    }
-})
+        } else if (req.query.max != undefined) {
+            res.json(error('Wrong value'))
 
-app.get('/api/v1/users', (req, res) => {
-    if (req.query.max != undefined && req.query.max > 0) {
-        res.json(success(users.slice(0, req.query.max))) //permet de mettre un nombre de resultat voulu
-
-    } else if (req.query.max != undefined) {
-        res.json(error('Wrong value'))
-
-    } else {
-        res.json(success(users))
-    }
-})
-
-//POST
-app.post('/api/v1/users', (req, res) => {
-    if (req.body.name) {
-        let sameName = false
-        for (let i = 0; i < users.length; i++) {
-            if (users[i].name == req.body.name) {
+        } else {
+            res.json(success(users))
+        }
+    })
+    //POST one user
+    .post((req, res) => {
+        if (req.body.name) {
+            let sameName = false
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].name == req.body.name) {
+                    res.json(error("Name already in use"))
+                    sameName = true
+                    break
+                }
+            }
+            if (sameName) {
                 res.json(error("Name already in use"))
-                sameName = true
-                break
+            } else {
+                let user = {
+                    id: createId(),
+                    name: req.body.name
+                }
+                users.push(user)
+                res.json(success(user))
             }
-        }
-        if (sameName) {
-            res.json(error("Name already in use"))
         } else {
-            let user = {
-                id: createId(),
-                name: req.body.name
-            }
-            users.push(user)
-            res.json(success(user))
+            res.json(error('No name value'))
         }
-    } else {
-        res.json(error('No name value'))
-    }
-})
+    })
 
-//PUT
-app.put('/api/v1/users/:id', (req, res) => {
-
-    let index = getIndex(req.params.id);
-    if (typeof (index) == 'string') {
-        res.json(error(index))
-    } else {
-
-        let same = false
-
-        for (let i = 0; i < users.length; i++) {
-            if (req.body.name == users[i].name && req.params.id != users[i].id) {
-                same = true
-                break
-            }
-        }
-        if (same) {
-            res.json(error('Same name'))
-
+UsersRouter.route('/:id') //Routeur
+    //GET one member with id
+    .get((req, res) => {
+        let index = getIndex(req.params.id);
+        if (typeof (index) == 'string') {
+            res.json(error(index))
         } else {
-            users[index].name = req.body.name
-            res.json(success(true))
-
+            res.json(success(users[index]))
         }
-    }
+    })
+    //PUT modify one user with id
+    .put((req, res) => {
+        let index = getIndex(req.params.id);
+        if (typeof (index) == 'string') {
+            res.json(error(index))
+        } else {
+            let same = false
+            for (let i = 0; i < users.length; i++) {
+                if (req.body.name == users[i].name && req.params.id != users[i].id) {
+                    same = true
+                    break
+                }
+            }
+            if (same) {
+                res.json(error('Same name'))
 
+            } else {
+                users[index].name = req.body.name
+                res.json(success(true))
+            }
+        }
+    })
+    //DELETE one user
+    .delete((req, res) => {
 
-})
+        let index = getIndex(req.params.id);
+        if (typeof (index) == 'string') {
+            res.json(error(index))
+        } else {
+            users.splice(index, 1)
+            res.json(success(users))
+        }
+    })
 
-//DELETE
-
-app.delete('/api/v1/users/:id', (req, res) => {
-
-    let index = getIndex(req.params.id);
-    if (typeof (index) == 'string') {
-        res.json(error(index))
-    } else {
-        users.splice(index, 1)
-        res.json(success(users))
-    }
-
-})
-
+app.use('/api/v1/users', UsersRouter)
 
 app.listen('8080', () => {
     console.log('listening on 8080')
