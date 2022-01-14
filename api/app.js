8274e5d9e5c6eb310c0d585e5f09affbd136ea54
melissa98m <melissa.mangione@gmail.com>
2022-01-14 11:00:15 +0100

    ajout d'un user

--- api/app.js
+++ api/app.js
@@ -1,22 +1,149 @@
-const {success, error} = require('functions')
+const {success, error} = require('./functions')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
 const config = require('./config')
-const users = [
-    {
-        id: 1,
-        name: "Mélissa"
-    },
-    {
-        id: 2,
-        name: "John"
-    },
-    {
-        id: 3,
-        name: "Fabien"
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    database: 'nodejs',
+    user: 'root',
+    password: 'tiger',
+})
+
+db.connect((err) => {
+    if (err) {
+        console.log(err.message)
+    } else {
+        console.log("Connection established")
+
+        const app = express();
+
+        let UsersRouter = express.Router()
+
+        app.use(morgan('dev'))
+        app.use(express.json()) // for parsing application/json
+        app.use(express.urlencoded({extended: true})) // for parsing application/x-www-form-urlencoded
+
+        UsersRouter.route('/')
+            //GET all user
+            .get((req, res) => {
+                if (req.query.max != undefined && req.query.max > 0) {
+                    db.query('SELECT * FROM users LIMIT 0,?', [req.query.max], (err, result) => {
+                        if (err) {
+                            res.json(error(err.message))
+                        } else {
+                            res.json(success(result))
+                        }
+                    })
+                } else if (req.query.max != undefined) {
+                    res.json(error('Wrong value'))
+                } else {
+                    db.query('SELECT * FROM users', (err, result) => {
+                        if (err) {
+                            res.json(error(err.message))
+                        } else {
+                            res.json(success(result))
+                        }
+                    })
+                }
+            })
+            //POST one user
+            .post((req, res) => {
+                if (req.body.name) {
+
+                    db.query('SELECT * FROM users WHERE name = ?', [req.body.name], (err, result) => { //verifie si le nom est pas deja pris
+                        if (err) {
+                            res.json(error(err.message))
+                        } else {
+                            if (result[0] != undefined) {
+                                res.json(error('Name already in use'))
+                            } else {
+                                db.query('INSERT INTO users(name) VALUES (?)', [req.body.name], (err, result) => { //insert le nouveau user
+                                    if (err) {
+                                        res.json(error(err.message))
+                                    } else {
+                                        db.query('SELECT * FROM users WHERE name = ?', [req.body.name], (err, result) => { // on recupere ensuite sans id
+                                            if (err) {
+                                                res.json(error(err.message))
+                                            } else {
+                                                res.json(success({  //on envoi le succes avec le nom et l'id
+                                                    id: result[0].id,
+                                                    name: result[0].name
+                                                }))
+                                            }
+                                        })
+
+                                    }
+                                })
+
+                            }
+
+                        }
+                    })
+                }
+            })
+
+        UsersRouter.route('/:id') //Routeur
+            //GET one member with id
+            .get((req, res) => {
+                db.query("SELECT * FROM members WHERE id= ?", [req.params.id], (err, result) => {
+                    if (err) {
+                        res.json(error(err.message))
+                    } else {
+                        if (result[0] != undefined) {
+                            res.json(success(result[0]))
+                        } else {
+                            res.json(error("Wrong id value"))
+                        }
+                    }
+                })
+
+            })
+            //PUT modify one user with id
+        .put((req, res) => {
+                let index = getIndex(req.params.id);
+                if (typeof (index) == 'string') {
+                    res.json(error(index))
+                } else {
+                    let same = false
+                    for (let i = 0; i < users.length; i++) {
+                        if (req.body.name == users[i].name && req.params.id != users[i].id) {
+                            same = true
+                            break
+                        }
+                    }
+                    if (same) {
+                        res.json(error('Same name'))
+
+                    } else {
+                        users[index].name = req.body.name
+                        res.json(success(true))
+                    }
+                }
+            })
+            //DELETE one user
+            .delete((req, res) => {
+
+                let index = getIndex(req.params.id);
+                if (typeof (index) == 'string') {
+                    res.json(error(index))
+                } else {
+                    users.splice(index, 1)
+                    res.json(success(users))
+                }
+            })
+
+        app.use(config.rootAPI + '/users', UsersRouter)
+
+        app.listen(config.port, () => {
+            console.log('listening on 8080')
+        })
+
     }
-]
+})
+
 
 //middlewere programe qui s'execute avant le reste
 /*app.use((req , res , next) => {
@@ -41,100 +168,7 @@ app.listen('8080', () => {
     console.log('listening on 8080')
 })
 */
-let UsersRouter = express.Router()
 
-app.use(morgan('dev'))
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({extended: true})) // for parsing application/x-www-form-urlencoded
-
-UsersRouter.route('/')
-    //GET all user
-    .get((req, res) => {
-        if (req.query.max != undefined && req.query.max > 0) {
-            res.json(success(users.slice(0, req.query.max))) //permet de mettre un nombre de resultat voulu
-
-        } else if (req.query.max != undefined) {
-            res.json(error('Wrong value'))
-
-        } else {
-            res.json(success(users))
-        }
-    })
-    //POST one user
-    .post((req, res) => {
-        if (req.body.name) {
-            let sameName = false
-            for (let i = 0; i < users.length; i++) {
-                if (users[i].name == req.body.name) {
-                    res.json(error("Name already in use"))
-                    sameName = true
-                    break
-                }
-            }
-            if (sameName) {
-                res.json(error("Name already in use"))
-            } else {
-                let user = {
-                    id: createId(),
-                    name: req.body.name
-                }
-                users.push(user)
-                res.json(success(user))
-            }
-        } else {
-            res.json(error('No name value'))
-        }
-    })
-
-UsersRouter.route('/:id') //Routeur
-    //GET one member with id
-    .get((req, res) => {
-        let index = getIndex(req.params.id);
-        if (typeof (index) == 'string') {
-            res.json(error(index))
-        } else {
-            res.json(success(users[index]))
-        }
-    })
-    //PUT modify one user with id
-    .put((req, res) => {
-        let index = getIndex(req.params.id);
-        if (typeof (index) == 'string') {
-            res.json(error(index))
-        } else {
-            let same = false
-            for (let i = 0; i < users.length; i++) {
-                if (req.body.name == users[i].name && req.params.id != users[i].id) {
-                    same = true
-                    break
-                }
-            }
-            if (same) {
-                res.json(error('Same name'))
-
-            } else {
-                users[index].name = req.body.name
-                res.json(success(true))
-            }
-        }
-    })
-    //DELETE one user
-    .delete((req, res) => {
-
-        let index = getIndex(req.params.id);
-        if (typeof (index) == 'string') {
-            res.json(error(index))
-        } else {
-            users.splice(index, 1)
-            res.json(success(users))
-        }
-    })
-
-app.use(config.rootAPI+'/users', UsersRouter)
-
-app.listen(config.port , () => {
-    console.log('listening on 8080')
-})
 
 function getIndex(id) {  //fonction pour avoir l'index
     for (let i = 0; i < users.length; i++) {
@@ -149,6 +183,10 @@ function createId() {
     return lastUser = users[users.length - 1].id + 1
 
 }
+
+
+
+
 
 
 
