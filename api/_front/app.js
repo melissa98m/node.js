3f658e516017f342ec76d9b6f3fef0f98d5e8277
melissa98m <melissa.mangione@gmail.com>
2022-01-16 11:47:59 +0100

    front put et delete

--- api/_front/app.js
+++ api/_front/app.js
@@ -23,13 +23,11 @@ app.use(express.urlencoded({extended: true}));
 // Routes
 //Page accueil
 app.get('/', (req, res) => {
-    res.render('index.twig', {
-        name: req.params.name,
-    })
+    res.redirect('/users')
 })
 //Page tout les users
 app.get('/users/', (req, res) => {
-    apiCall(req.query.max ? '/users?max='+req.query.max :'/users', res, (result) => {
+    apiCall(req.query.max ? '/users?max='+req.query.max :'/users','get', {}, res, (result) => {// si il y a un req.query.max faire /users?max= sinon /users
         res.render('users.twig', {
             users: result
         })
@@ -37,10 +35,35 @@ app.get('/users/', (req, res) => {
 })
 //Page d'un user
 app.get('/users/:id', (req, res) => {
-    apiCall('/users/' + req.params.id, res, (result) => {
+    apiCall('/users/' + req.params.id,'get', {}, res, (result) => {
         res.render('oneUser.twig', {
             user: result
         })
+    })
+})
+
+//Page update user
+app.get('/users/edit/:id', (req, res) =>{
+    apiCall('/users/' + req.params.id, 'get', {}, res, (result) => {
+        res.render('editUser.twig', {
+            user: result
+        })
+    })
+})
+
+//Methode modifiant l'user
+app.post('/users/edit/:id', (req, res) => {
+    apiCall('/users/'+req.params.id, 'put', {
+        name: req.body.name,
+    }, res, () => {
+        res.redirect('/users')
+    })
+})
+
+//supprimer un user
+app.post('/delete', (req, res) => {
+    apiCall('/users/' + req.body.id, 'delete', {}, res, () => {
+        res.redirect('/users')
     })
 })
 //Lancement de l'app
@@ -52,18 +75,22 @@ app.get('/users/:id', (req, res) => {
             errorMsg: errMsg
         })
     }
-        function apiCall(url, res, next) {
-            fetch.get(url)
-                .then((response) => {
-                    if (response.data.status == 'success') {
-                        next(response.data.result)
-                    } else {
-                        renderError(res, response.data.message)
-                    }
-                })
-                .catch((err) => {
-                    renderError(res, err.message)
-                })
 
+function apiCall(url, method, data, res, next) {
+    fetch({
+        method: method,
+        url: url,
+        data: data
+    }).then((response) => {
+        if (response.data.status == 'success') {
+            next(response.data.result)
+        } else {
+            renderError(res, response.data.message)
         }
+    })
+        .catch((err) => {
+            renderError(res, err.message)
+        })
+
+}
 
